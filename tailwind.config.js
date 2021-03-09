fddc6ff5c325968da8e733c7081489802b2fa8d6
Sander Byrkjeland <sunbyr@gmail.com>
2021-03-09 01:31:49 +0100

    added colors to tailwind config

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,6 +2,27 @@ module.exports = {
 	purge: ["./src/**/*.{js,jsx,ts,tsx}", "./public/index.html"],
 	darkMode: false, // or 'media' or 'class'
 	theme: {
+		colors: {
+			teal: {
+				light: "#00FFE5",
+				DEFAULT: "#00bdaa",
+				dark: "#1BA093",
+			},
+			gray: {
+				lightest: "#b1abab",
+				light: "#525252",
+				DEFAULT: "#333333",
+			},
+			black: {
+				DEFAULT: "#020202",
+			},
+			orange: {
+				DEFAULT: "#fd8b25",
+			},
+			white: {
+				DEFAULT: "#ffffff",
+			},
+		},
 		extend: {},
 	},
 	variants: {
