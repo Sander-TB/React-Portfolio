430852c7ee3ac19a99151caf3019a35657ba0a4f
Sander Byrkjeland <sunbyr@gmail.com>
2021-03-15 17:46:07 +0100

    WIP: Skeleton of blog page done, real data and layout next

--- src/pages/Blog.js
+++ src/pages/Blog.js
@@ -1,3 +1,20 @@
+import Heading from "../components/layout/Heading";
+
 export default function Blog() {
-	return <h1>hello blog</h1>;
+	return (
+		<>
+			<Heading content='Blog' />
+			<div className='flex'>
+				<main>
+					<h1>Post title</h1>
+					<p>Date</p>
+					<p>Blurb</p>
+					<button>Read More</button>
+				</main>
+				<aside>
+					<h3>Tags:</h3>
+				</aside>
+			</div>
+		</>
+	);
 }
